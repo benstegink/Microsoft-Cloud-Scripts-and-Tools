c02baa46029a4b82f37dd7befd78ce2620088291
Ben Stegink | Microsoft MVP, MCT <benstegink@users.noreply.github.com>
2024-09-04 16:38:20 -0400

    Update 01-ConfigureSP2016.ps1

--- SharePoint 2016/01-ConfigureSP2016.ps1
+++ SharePoint 2016/01-ConfigureSP2016.ps1
@@ -3,12 +3,13 @@ $ConfigDB = "SP2016_Config"
 $AdminContentDB = "SP2016_Content_Admin"
 $Passphrase = "sh@r3p01nt"
 $distCacheHost = $false
+$role = "ApplicationWithSearch"
 
 $farmadminCred = Get-Credential
 
 if($distCacheHost -eq $false){
     New-SPConfigurationDatabase -DatabaseServer $DBServer -DatabaseName $ConfigDB -AdministrationContentDatabaseName $AdminContentDB `
--FarmCredentials $farmadminCred -Passphrase (ConvertTo-SecureString $PassPhrase –AsPlaintext –Force) -LocalServerRole ApplicationWithSearch -SkipRegisterAsDistributedCacheHost
+-FarmCredentials $farmadminCred -Passphrase (ConvertTo-SecureString $PassPhrase –AsPlaintext –Force) -LocalServerRole $role -SkipRegisterAsDistributedCacheHost
 
 }
 elseif($distCacheHost -eq $true){
